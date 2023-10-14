cf9a87a0ffeedecca1da0f29144b758fee803120
Abrorjon Asralov <asralov@arizona.edu>
2023-10-14 00:18:11 -0700

    Created left personal info box
    Have created left box where i showcase a short desc about me

--- portfolio/public/src/index.js
+++ portfolio/public/src/index.js
@@ -1,21 +1,8 @@
+
 // to make the sidebar closed by default
 let isOpen = false;
 const btn = document.getElementById("menuButton");
-function showSideBar() {
-  const menuBar = document.getElementById("navbarLinks");
-  // if it is closed, and is about to be open
-  if (!isOpen) {
-    btn.style.transform = "translateX(0)";
-    menuBar.style.transform = "translateX(0)";
-    isOpen = true;
-  } else {
-    btn.style.transform = "translateX(-280px)";
-    menuBar.style.transform = "translateX(-280px)";
-    isOpen = false;
-  }
-}
 const menuBar = document.getElementById("navbarLinks");
-
 btn.addEventListener('click', ()=>{
   if (!isOpen) {
     menuBar.style.left = 0;
@@ -29,3 +16,16 @@ btn.addEventListener('click', ()=>{
   }
   console.log(isOpen)
 });
+
+// to show the string that is the personal info about me
+const info = `Welcome to My Tech Journey<br>
+<br>
+Hello, I'm Abrorjon Asralov, a student web developer with a strong passion for technology. I'm currently pursuing a Computer Science degree at the University of Arizona.<br>
+<br>
+I'm dedicated to creating responsive and user-friendly websites using HTML, CSS, and JavaScript. My technical skills extend to Java, Python, C#, and various frameworks.<br>
+<br>
+I'm also a team player, a quick problem solver, and a great communicator. With a background in volunteer work, I'm committed to inspiring the next generation of students to reach for their dreams.<br>
+<br>
+Explore my work, connect on LinkedIn, or check out my projects on GitHub. Let's connect and create something amazing together.`;
+const leftInfoBox = document.getElementById("personalInfoText");
+leftInfoBox.innerHTML   = info;
